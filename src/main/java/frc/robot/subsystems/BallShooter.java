890ab9c0c8c207ffd4040f3b877937879163d6e2
smabotics <smabotics.ag@smanet.org>
2019-01-26 15:34:22 -0700

    initial commit

---
+++ src/main/java/frc/robot/subsystems/BallShooter.java
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.Robot;
+import frc.robot.RobotMap;
+
+/**
+ * Add your docs here.
+ */
+public class BallShooter extends Subsystem {
+
+  private WPI_TalonSRX ballShooterLeft;
+  private WPI_TalonSRX ballShooterRight;
+
+  public void ballShooter() {
+    ballShooterLeft = new WPI_TalonSRX(RobotMap.BALL_SHOOTER_LEFT);
+    ballShooterRight = new WPI_TalonSRX(RobotMap.BALL_SHOOTER_RIGHT);
+  }
+
+  public void intake(){
+    Robot.ballShooter.ballShooterLeft.set(0.4);
+    Robot.ballShooter.ballShooterRight.set(0.4);
+  }
+
+  public void outtake(){
+    Robot.ballShooter.ballShooterLeft.set(-0.4);
+    Robot.ballShooter.ballShooterRight.set(-0.4);
+  }
+  
+  @Override
+  public void initDefaultCommand() {
+   
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/HatchSolenoid.java
@@ -0,0 +1,25 @@
+/*package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import frc.robot.RobotMap;
+
+
+public class HatchSolenoid extends Subsystem {
+  
+  DoubleSolenoid solenoid = new DoubleSolenoid(RobotMap.SOLENOID_FORWARD, RobotMap.SOLENOID_REVERSE);
+
+  public void forward(){
+    solenoid.set(DoubleSolenoid.Value.kForward);
+  }
+
+  public void reverse(){
+    solenoid.set(DoubleSolenoid.Value.kReverse);
+  }
+ 
+  @Override
+  public void initDefaultCommand() {
+   solenoid.set(DoubleSolenoid.Value.kOff);
+  }
+}
+*/
