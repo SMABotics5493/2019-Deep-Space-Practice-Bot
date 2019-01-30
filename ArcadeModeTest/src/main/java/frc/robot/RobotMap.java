822a1345935ce77d7fb73fb66612fa1bd796c7ec
smabotics <smabotics.ag@smanet.org>
2019-01-29 17:30:52 -0700

    juliaarcademode

---
+++ ArcadeModeTest/src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,44 @@
+package frc.robot;
+
+
+public class RobotMap {
+
+  public static final int DRIVE_JOYSTICK = 0;
+  public static final int JOYSTICK = 1;
+
+  public static final int JOYB_A = 1;
+	public static final int JOYB_B = 2;
+	public static final int JOYB_X = 3;
+	public static final int JOYB_Y = 4;
+	public static final int JOYB_LB = 5;
+	public static final int JOYB_RB = 6;
+	public static final int JOYB_LT = 7;
+  public static final int JOYB_RT = 8;
+  
+  public static final int RIGHTYAXIS = 5;
+  public static final int LEFTYAXIS = 6;
+  public static final int LEFTXAXIS = 0;
+  public static final int RIGHTXAXIS = 4;
+
+  public static final int LEFT_FRONT_MOTOR = 6;
+  public static final int RIGHT_FRONT_MOTOR = 4;
+  public static final int LEFT_BACK_MOTOR = 22;
+  public static final int RIGHT_BACK_MOTOR = 7;
+
+  public static final int BALL_SHOOTER_LEFT = 21;
+  public static final int BALL_SHOOTER_RIGHT = 5;
+
+  public static final int SOLENOID_FORWARD = 0;
+  public static final int SOLENOID_REVERSE = 1;
+
+  public static final int HATCH_PANEL =  9;
+  public static final int BALL_SHOOTER_LIFT = 8;
+
+  public static int ENCODER_LEFTA = 1;
+	public static int ENCODER_LEFTB = 2;
+	public static int ENCODER_RIGHTA = 5;
+	public static int ENCODER_RIGHTB = 6;
+	
+
+}
+
