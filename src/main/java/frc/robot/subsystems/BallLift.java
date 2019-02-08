2b7906b336a748556db4340fa1b48bf7dd3dd707
smabotics <smabotics.ag@smanet.org>
2019-02-08 16:46:20 -0700

    Lift Encoders

--- src/main/java/frc/robot/subsystems/BallLift.java
+++ src/main/java/frc/robot/subsystems/BallLift.java
@@ -3,17 +3,22 @@ package frc.robot.subsystems;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.CounterBase.EncodingType;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Robot;
 import frc.robot.RobotMap;
 
 public class BallLift extends Subsystem {
 
   public WPI_TalonSRX ballLiftMotor;
+  public Encoder ballEncoder;
 
   public BallLift(){
     super();
 
     ballLiftMotor = new WPI_TalonSRX(RobotMap.BALL_SHOOTER_LIFT);
+    ballEncoder = new Encoder(RobotMap.ENCODER_BALL_SHOOTERA,RobotMap.ENCODER_BALL_SHOOTERB, true, EncodingType.k4X);
   }
 
   public void liftUp(){
@@ -25,7 +30,18 @@ public class BallLift extends Subsystem {
   public void end(){
     Robot.ballLift.ballLiftMotor.set(0.05);
   }
+  public void ballValue() {
+		reset();
+			SmartDashboard.putNumber("Hatch Value", ballEncoder.getDistance());
+			//SmartDashboard.putNumber("Right Raw Count", rightEncoder.getRaw());
+			//SmartDashboard.putNumber("Left Raw Count", leftEncoder.getRaw());
+		reset();
+  }
+  private void reset() {
+    ballEncoder.reset();
+  }
 
+  
   @Override
   public void initDefaultCommand() {
 
