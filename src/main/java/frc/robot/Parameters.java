9c209d5a403b3a92e30d1182334e41bb4c2c1b93
smabotics <smabotics.ag@smanet.org>
2019-02-19 09:21:11 -0700

    fixed parameters error

--- src/main/java/frc/robot/Parameters.java
+++ src/main/java/frc/robot/Parameters.java
@@ -3,6 +3,11 @@ package frc.robot;
 
 public class Parameters {
     public static final double POVspeed = 1;
-
+    public static final double SLOWANGLE= 20;
+    public static final double SLOWDISTANCE= 600;
+    public static final double SLOWSPEED = 0.45;
+    public static final double MAXSPEED = 0.6;
+    public static final double ENCODERSPERINCH = 111.1;
+        
 }
 
