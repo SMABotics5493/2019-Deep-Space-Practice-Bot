78ff7529a457510923864c5ab1670f1623e54836
smabotics <smabotics.ag@smanet.org>
2019-09-18 13:02:41 -0600

    Added code to PID commmand from previous source, only edited in PIDCommand though

--- src/main/java/frc/robot/commands/PIDCommand.java
+++ src/main/java/frc/robot/commands/PIDCommand.java
@@ -7,7 +7,13 @@
 
 package frc.robot.commands;
 
+import javax.swing.text.AbstractDocument.LeafElement;
+
+import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import frc.robot.Robot;
+import frc.robot.subsystems.DriveBase;
 
 public class PIDCommand extends Command {
   public PIDCommand() {
@@ -21,19 +27,54 @@ public class PIDCommand extends Command {
     //set to false once you are done turning the PID
     final boolean DEBUG = true;
 
+    //PID controllers for each side of the drive train
+    private PIDController leftPID;
+    private PIDController rightPID;
+
+    //How far each side should go
+    private double leftDist;
+    private double rightDist;
+
+    //Drive subsystem that will expose motor control and distance traveled
+    private DriveBase drive;
+
+    //Used for diagnostic output to see how far each side was driven
+    private double leftStart;
+    private double rightStart;
 
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
+    this.drive = drive;
+    requires(Robot.driveBase);
+    //how far to travel
+    leftDist = leftM;
+    rightDist = rightM;
   }
 
   // Called just before this Command runs the first time
   @Override
   protected void initialize() {
+    if (DEBUG); {
+      SmartDashboard.putData("Left PID", leftPID);
+      SmartDashboard.putData("Right PID", rightPID);
+    }
   }
 
   // Called repeatedly when this Command is scheduled to run
   @Override
   protected void execute() {
+    double error;
+    double prevError;
+      while (PIDbutton.whileheld())
+    error = setpoint - encoder;
+    integral = integral + error;
+    if (error > 2)
+    integral = 0;
+      double derivative = error - prevError;
+    prevError = error;
+    Object power = error*Kp + integral*Ki + derivative*Kd;
+  }
+
   }
 
   // Make this return true when this Command no longer needs to run execute()
